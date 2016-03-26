1469321db4a4318f800be8111eb5caf055b9073b
Nick Jacobs <nick.mjac@gmail.com>
2016-03-27 11:15:37 +1300

    add javascript to racer game

--- racer.js
+++ racer.js
@@ -1,42 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 //getting the tracks for each player
 var lane1 = document.getElementById("lane1")
 var lane2 = document.getElementById("lane2")
+//get a length from the user to make track
+var tracklength = prompt("How far do you want to race? 3-10?")
 
+//build track
+for (var j = 0; j < tracklength; j++) {
+    lane1.appendChild(document.createElement('td'))
+    lane2.appendChild(document.createElement('td'))
+}
+
+//initialise each player to a track
 var player1 = lane1.querySelectorAll('td')
 var player2 = lane2.querySelectorAll('td')
 
-//initialising player positions on track
+//insert finish line
+player1[tracklength].className = "finish"
+player2[tracklength].className = 'finish'
+
+//initialise player positions on track
 var p1 = 0
 var p2 = 0
 
-document.addEventListener('keyup', updatePlayerPosition)
+var tinaWins = document.getElementById('tina_wins')
+var bobWins = document.getElementById('bob_wins')
 
-function updatePlayerPosition() {
+document.addEventListener('keyup', choosePlayer)
+
+
+function choosePlayer() {
     if (event.which === 90) {
-        if (player1[p1 + 1].className === "finish") {
-            player1[p1].className = ""
-            player1[p1 + 1].className = "active"
-            alert("Ugggghh Tina wins!")
-
-        }
-        if (player1[p1].className === "active") {
-            player1[p1].className = ""
-            player1[p1 + 1].className = "active"
-        }
+        updatePlayerPosition(player1, p1)
         p1++
+        if (player1[tracklength].className === "active") {
+            tinaWins.innerHTML = "Uhhhhhhh Tina wins! You get all the butts."
+            document.removeEventListener('keyup', choosePlayer)
+        }
     }
-
     else if (event.which === 77) {
-        if (player2[p2 + 1].className === "finish") {
-            player2[p2].className = ""
-            player2[p2 + 1].className = "active"
-            alert("Oh my god, Bob wins!")
-
-        }
-        if (player2[p2].className === "active") {
-            player2[p2].className = ""
-            player2[p2 + 1].className = "active"
-        }
+        updatePlayerPosition(player2, p2)
         p2++
+        if (player2[tracklength].className === "active") {
+            bobWins.innerHTML = "Oh my god! Bob wins!"
+            document.removeEventListener('keyup', choosePlayer)
+        }
     }
 }
+
+function updatePlayerPosition(player, p) {
+    if (player[p].className === "active") {
+        player[p].className = ""
+        player[p + 1].className = "active"
+    }
+}
+
+})
